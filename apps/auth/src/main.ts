4698d3df20a35a4d57bc728f07de0c98261c5b98
rostyslav48 <rostyslavkravchuk05@gmail.com>
2025-05-11 16:24:34 +0300

    feat/use constants in microservices main.ts

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,13 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 
+import { AuthModule } from './auth.module';
+
 import { RmqService } from '@app/common';
 
-import { AuthModule } from './auth.module';
+import { AUTH_SERVICE } from '@app/wardrobe-api-gateway/constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
   const rmqService = app.get<RmqService>(RmqService);
-  app.connectMicroservice(rmqService.getOptions('AUTH'));
+  app.connectMicroservice(rmqService.getOptions(AUTH_SERVICE));
   await app.startAllMicroservices();
 }
 
--- apps/wardrobe/src/main.ts
+++ apps/wardrobe/src/main.ts
@@ -1,14 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 
+import { WardrobeModule } from './wardrobe.module';
+
 import { RmqService } from '@app/common';
 
-import { WardrobeModule } from './wardrobe.module';
+import { WARDROBE_SERVICE } from '@app/wardrobe-api-gateway/constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(WardrobeModule);
   const rmqService = app.get<RmqService>(RmqService);
-  app.connectMicroservice(rmqService.getOptions('WARDROBE'));
+  app.connectMicroservice(rmqService.getOptions(WARDROBE_SERVICE));
   app.useGlobalPipes(new ValidationPipe());
   await app.startAllMicroservices();
 }
